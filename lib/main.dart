bcae60ff07a2365b72875f12714b72718bdbe6de
hsimao <e087754958@gmail.com>
2023-12-13 21:13:25 +0800

    feat: get toons data in home screen and use FutureBuilder

--- lib/main.dart
+++ lib/main.dart
@@ -1,9 +1,7 @@
 import 'package:comic/screens/home_screen.dart';
-import 'package:comic/services/api_service.dart';
 import 'package:flutter/material.dart';
 
 void main() {
-  ApiService().getTodaysToons();
   runApp(const App());
 }
 
@@ -12,7 +10,7 @@ class App extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const MaterialApp(
+    return MaterialApp(
       home: HomeScreen(),
     );
   }
--- lib/screens/home_screen.dart
+++ lib/screens/home_screen.dart
@@ -1,7 +1,11 @@
+import 'package:comic/models/webtoon_model.dart';
+import 'package:comic/services/api_service.dart';
 import 'package:flutter/material.dart';
 
 class HomeScreen extends StatelessWidget {
-  const HomeScreen({super.key});
+  HomeScreen({super.key});
+
+  Future<List<WebtoonModel>> webtoons = ApiService.getTodaysToons();
 
   @override
   Widget build(BuildContext context) {
@@ -18,6 +22,12 @@ class HomeScreen extends StatelessWidget {
           ),
         ),
       ),
+      body: FutureBuilder(
+          future: webtoons,
+          builder: (context, snapshot) {
+            if (snapshot.hasData) return const Text('There is data!');
+            return const Text("Loading...");
+          }),
     );
   }
 }
