06d06649a2b58f663d4d8c9abb68b42bc26e92d4
hsimao <e087754958@gmail.com>
2023-12-14 14:53:45 +0800

    feat: webtoon card

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,21 @@
+import 'dart:io';
+
 import 'package:comic/screens/home_screen.dart';
 import 'package:flutter/material.dart';
 
+class MyHttpOverrides extends HttpOverrides {
+  @override
+  HttpClient createHttpClient(SecurityContext? context) {
+    return super.createHttpClient(context)
+      ..userAgent =
+          'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/110.0.0.0 Safari/537.36';
+  }
+}
+
 void main() {
+  // NOTE: fix image fetch issue
+  HttpOverrides.global = MyHttpOverrides();
+
   runApp(const App());
 }
 
